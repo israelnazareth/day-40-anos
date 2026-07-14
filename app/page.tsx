a605883838b78028360040cfee440afa2b578cb0
Israel Nazareth <israelsnazareth@gmail.com>
2026-07-14 13:54:40 -0300

    fix: remove dynamic const

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,6 @@
 import { getEventBySlug } from "@/lib/db/queries/events";
 import { Homepage } from "./homepage";
 
-export const dynamic = "force-dynamic";
-
 export default async function Home() {
   const event = await getEventBySlug("day-40-anos");
 
