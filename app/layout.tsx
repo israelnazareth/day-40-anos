cfd494e3041b4309bc7f181252f35937f69b6961
Israel Nazareth <israelsnazareth@gmail.com>
2026-07-17 09:20:12 -0300

    feat: update metadata title for improved SEO and branding

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,7 @@ const geistMono = Geist_Mono({
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://day-40-anos.vercel.app"),
+  title: "Day Nazareth • 40 anos",
   openGraph: {
     title: "Day Nazareth • 40 anos",
     description:
