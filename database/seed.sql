8bf477275e21fddeb0a4301f478a1a5f4844d154
Israel Nazareth <israelsnazareth@gmail.com>
2026-09-02 16:05:03 -0300

    feat: add day40anos image assets for milestone celebration feature

--- database/seed.sql
+++ database/seed.sql
@@ -28,7 +28,7 @@ INSERT INTO events (
 
 VALUES (
     'day-40-anos',
-    'Day Nazareth - 40 anos',
+    'Day Muniz - 40 anos',
     'Você está convidado(a)',
     'Uma celebração especial para comemorar 40 anos de história e tradição.',
     '2026-10-02T20:00:00-03:00',
@@ -37,7 +37,7 @@ VALUES (
     'https://www.google.com/maps/search/?api=1&query=Espa%C3%A7o+Zarifi+Rua+das+Margaridas+335+Vila+Valqueire+Rio+de+Janeiro',
     'https://www.google.com/maps?q=Espa%C3%A7o%20Zarifi%2C%20Rua%20das%20Margaridas%20335%2C%20Vila%20Valqueire%2C%20Rio%20de%20Janeiro&output=embed',
     '+55 21 98708-6134',
-    'Day Nazareth',
+    'Day Muniz',
     '+55 21 98708-6134'
 );
 
