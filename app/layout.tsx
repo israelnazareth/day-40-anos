c175df6c58f1955ce7dfbc34724c3b037dff51aa
Israel Nazareth <israelsnazareth@gmail.com>
2026-09-02 16:41:48 -0300

    feat: remove width and height properties from OpenGraph image configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,8 +30,6 @@ export const metadata: Metadata = {
     images: [
       {
         url: "/og-image.webp",
-        width: 1200,
-        height: 630,
         alt: "Day Muniz • 40 anos",
       },
     ],
